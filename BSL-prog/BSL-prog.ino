2e7bca59b1de9630605e59635421901c3371df1e
Petr <shapovalov_petya@mail.ru>
2025-10-28 11:58:45 +0300

    fix code
    Поменял распиновку + увеличил частоту ШИМ, чтобы транзистор не пищал

--- BSL-prog/BSL-prog.ino
+++ BSL-prog/BSL-prog.ino
@@ -1,33 +1,49 @@
-#define SENSOR_PIN  PD2   // Дискретный вход
-#define LED_PIN     PB2   // ШИМ
-#define TIMEOUT     (20 * 60 * 1000UL)  // Время работы лампы, мс
-#define LED_POWER   50  // Мощность ШИМ, %
+#include <util/delay.h>
 
-int pwmPower        = 0;
-long int time       = 0;
+#define SENSOR_PIN  4   // Дискретный вход
+#define LED_PIN     3   // ШИМ
+
+#define SECONDS     0
+#define MINUTES     0
+#define TIMEOUT     ((MINUTES * 60 + SECONDS) * 1000UL)  // Время работы лампы, мс
+
+long time           = 0;
 bool ledIsWork      = false;
+void testBlink();
 
 void setup() {
   pinMode(SENSOR_PIN, INPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  TCCR2A |= _BV(WGM20) | _BV(WGM21);
+  TCCR2B =  (TCCR2B & 0b11111000) | 0x01;
   pinMode(LED_PIN, OUTPUT);
 
-  pwmPower = 255 * LED_POWER / 100;
   analogWrite(LED_PIN, 0);
 }
 
 void loop() {
-  if (digitalRead(SENSOR_PIN)) {
+  if (digitalRead(SENSOR_PIN) == LOW) {
     time = millis();
     if (!ledIsWork) {
-      analogWrite(LED_PIN, pwmPower);
+      analogWrite(LED_PIN, 1);
       ledIsWork = true;
     }
+    // testBlink();
   }
+  
 
-  if (ledIsWork && (millis() - time >= TIMEOUT)) {
+  if (ledIsWork && (millis() - time > TIMEOUT)) {
     analogWrite(LED_PIN, 0);
     ledIsWork = false;
   }
 
-  delay(100);
+  _delay_ms(500);
+}
+
+
+void testBlink() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(50);
+  digitalWrite(LED_BUILTIN, LOW);
 }
